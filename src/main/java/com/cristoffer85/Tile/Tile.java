f4e939547b4808420f57f710e50fc2764b669dbf
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2025-01-29 12:24:25 +0100

    Improved base TileSheet

--- src/main/java/com/cristoffer85/Tile/Tile.java
+++ src/main/java/com/cristoffer85/Tile/Tile.java
@@ -15,7 +15,20 @@ public class Tile {
 
     // COLLIDABLE TILES values go here (from array value from tilesheet)
     static {
-        collidableTileIds.add(3);
+        collidableTileIds.add(1);   // Grass water top left corner
+        collidableTileIds.add(2);   // Grass water top center
+        collidableTileIds.add(3);   // Grass water top right corner
+        collidableTileIds.add(4);   // Grass water top left outer corner
+        collidableTileIds.add(5);   // Grass water top right outer corner
+        collidableTileIds.add(21);  // Grass water left center
+        collidableTileIds.add(23);  // Grass water right center
+        collidableTileIds.add(24);  // Grass water bottom left outer corner
+        collidableTileIds.add(25);  // Grass water bottom right outer corner
+        collidableTileIds.add(40);  // Water base
+        collidableTileIds.add(41);  // Grass water bottom left corner
+        collidableTileIds.add(42);  // Grass water bottom center
+        collidableTileIds.add(43);  // Grass water bottom right corner
+        collidableTileIds.add(80);  // Water waves
     }
 
     public Tile(BufferedImage image, int tileId, boolean collidable) {
